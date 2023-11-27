c8d9ce2c4088bf8488671a453d1fff8e88bc4426
tarde <tarde@A13E34>
2023-11-27 16:53:12 +0100

    commit corrección

--- 03_cuentas_bancarias/src/view/Banco.java
+++ 03_cuentas_bancarias/src/view/Banco.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.ArrayList;
+
 /*Pide el número de cuenta, saldo inicial y el límite después:
 1.- Ingresar
 2.- Extraer
@@ -8,67 +10,70 @@ package view;
 
 import java.util.Scanner;
 
-import service.Cuenta;
-import service.CuentaLimite;
+import model.Movimiento;
 import service.CuentaMovimientos;
 
 public class Banco {
 	static CuentaMovimientos cuentamov;
 	public static void main(String[] args) {
-		String codigo;
-		double saldo,limite;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Introduce tu código de cuenta: ");
-		codigo=sc.nextLine();
-		System.out.println("Por favor, introduce tu saldo inicial: ");
-		saldo=sc.nextDouble();
-		System.out.println("Por favor, introduce el límite: ");
-		limite=sc.nextDouble();
-		
-		int opcion;
-		do {	
-				mostrarMenu();
-				opcion=sc.nextInt();
-				switch(opcion) {
-				case 1:
-					System.out.println("Introduce la cantidad a ingresar");
-					cuenta.ingresar(sc.nextDouble());
-					System.out.println("Tu saldo actual es de: "+cuenta.getSaldo());
-					break;
-				case 2:
-					System.out.println("Introduce la cantidad que quieres extraer");
-					cuentaLimite.extraer(sc.nextDouble());
-					System.out.println("Tu saldo actual es de: "+cuentaLimite.getSaldo());
-					break;
-				case 3:
-					//mostrarSaldo();
-					break;
-				
-				case 4:
-					System.out.println("Gracias por fiarnos tu dinero");
-				}			
+		//pido codigo, saldo, pido límite y creo el objeto CuentaMovimientos
+				String codigo;
+				double saldo,limite;
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Introduce codigo de cuenta:");
+				codigo=sc.nextLine();
+				System.out.println("Introduce saldo:");
+				saldo=Double.parseDouble(sc.nextLine());
+				System.out.println("Introduce límite:");
+				limite=Double.parseDouble(sc.nextLine());
+				cuentamov=new CuentaMovimientos(codigo,saldo, limite);
+				//ahora viene el rollo menú...
+				int opcion;
+				do {
+					mostrarMenu();
+					opcion=sc.nextInt();//lee la opción elegida
+					switch(opcion) { //evaluamos
+						case 1:
+							ingresar();
+							break;
+						case 2:
+							extraer();
+							break;
+						case 3:
+							mostrarMovimientos();
+							break;				
+						case 4:
+							System.out.println("---Adios---");
+					}
+				}while(opcion!=4);	
+			}
 			
-		}
-		while(opcion!=4);
-	}
-	
-	static void mostrarMenu() {
-		System.out.println("1.Ingresar");
-		System.out.println("2.Extraer");
-		System.out.println("3.Mostrar saldo y últimos movimientos");
-		System.out.println("4.Salir");	
-	}
-	
-	static void ingresar() {
-		
-	}
-	
-	static void extraer() {
-		
-	}
-	
-	static void mostrarMovimientos() {
-		
-	}
-
+			//resto de métodos
+			static void mostrarMenu() {
+				System.out.println("1.- Ingresar dinero");
+				System.out.println("2.- Extraer dinero");
+				System.out.println("3.- Mostrar movimientos");
+				System.out.println("4.- Salir");		
+			}
+			static void ingresar() {
+				Scanner sc=new Scanner(System.in);
+				double cantidad;
+				System.out.println("Introduce cantidad");
+				cantidad=Double.parseDouble(sc.nextLine());
+				cuentamov.ingresar(cantidad);
+			}
+			static void extraer() {
+				Scanner sc=new Scanner(System.in);
+				double cantidad;
+				System.out.println("Introduce cantidad");
+				cantidad=Double.parseDouble(sc.nextLine());
+				cuentamov.extraer(cantidad);
+			}
+			static void mostrarMovimientos() {
+				ArrayList<Movimiento> movs=cuentamov.obtenerMovimientos();
+				for(Movimiento m:movs) {
+					System.out.println("Tipo:"+m.getTipo()+" Cantiad:"+m.getCantidad()+" Fecha:"+m.getFechaMovimiento());
+				}
+				System.out.println("Saldo final: "+cuentamov.getSaldo());
+			}
 }
